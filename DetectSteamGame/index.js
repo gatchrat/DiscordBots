b932fa0580ae646664524a32a62dab6a7ecd851c
Niko Loeffler <super.ikon@hotmail.de>
2020-03-19 19:00:52 +0100

    added reactions

--- DetectSteamGame/index.js
+++ DetectSteamGame/index.js
@@ -8,18 +8,24 @@ var checkTimerinSec = 10;
 
 var bot = new Discord.Client({
 	autorun: true
- });
- bot.login(settings.discord.botToken);
- bot.on('ready', function (evt) {
-    console.log('Connected');
-	setInterval(check, checkTimerinSec*1000);
 });
+bot.login(settings.discord.botToken);
+bot.on('ready', function (evt) {
+	console.log('Connected');
+	setInterval(check, checkTimerinSec * 1000);
+});
+
+async function asyncForEach(array, callback) {
+	for (let index = 0; index < array.length; index++) {
+	  await callback(array[index], index, array);
+	}
+  }
 
 function check() {
 	SteamApi.getPlayerInfo(settings.steam.profileID, settings.steam.apiKey, (err, data) => {
-		if(err) console.log(err);
-		if(data.gameid != null ){
-			if(data.gameid ==  settings.steam.gameID && !alreadyPlaying ){
+		if (err) console.log(err);
+		if (data.gameid != null) {
+			if (data.gameid == settings.steam.gameID && !alreadyPlaying) {
 				alreadyPlaying = true;
 				console.log("found one");
 				const channel = bot.channels.get(settings.discord.channelID);
@@ -28,19 +34,25 @@ function check() {
 					message += person.id + " ";
 				});
 				message += " " + settings.discord.message;
-				channel.send(message);
+				channel.send(message).then(async sendMessage => {
+					var reaction = settings.discord.reactions[Math.floor(Math.random() * settings.discord.reactions.length)];
+					await asyncForEach(reaction, async (r) => {
+						await sendMessage.react(r);
+					})
+				})
+				.catch(console.error);;
 			}
 		}
-		else{
+		else {
 			alreadyPlaying = false;
 		}
 	})
- }
-
- 
+}
 
 
- 
+
+
+
 
 
 
